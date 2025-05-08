a7bd7759ef47a95517159ec0ff124efba16504d9
jakesouleater <111393639+jakesouleater@users.noreply.github.com>
2025-05-08 00:29:05 -0400

    Add files via upload
    Added code to both html and JS files and fixed any errors with inside the code.

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
     .then((response) => response.json())
     .then((data) => {
         allPokemons = data.results;
+        displayPokemons(allPokemons);
     });
 
 async function fetchPokemonDataBeforeRedirect(id) {
     try {
-        const [pokemon, pokemonSpecies] = await promise.
+        const [pokemon, pokemonSpecies] = await Promise.
             all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
-                res.json
+                res.json()
         ),
         fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
-                res.json
+                res.json()
         ),
      ])
      return true
@@ -33,19 +34,65 @@ function displayPokemons(pokemon) {
     listWrapper.innerHTML = "";
 
     pokemon.forEach((pokemon) => {
-        const pokemonId = pokemon.url.split("/")[6];
+        const pokemonID = pokemon.url.split("/")[6];
         const listItem = document.createElement('div');
         listItem.className = "list-item";
         listItem.innerHTML = `
             <div class="number-wrap">
-                <p class="caption-fonts">#${pokemonId}</P>
+                <p class="caption-fonts">#${pokemonID}</P>
             </div>
             <div class="img-wrap">
-                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg" alt="${pokemon.name}" />
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
             </div>
-            <div class="number-wrap">
-                <p class="caption-fonts">#${pokemonId}</P>
+            <div class="name-wrap">
+                <p class="caption-fonts">#${pokemon.name}</P>
             </div>
-        `
-    })
+        `;
+
+        listItem.addEventListener("click", async () => {
+            const success  = await fetchPokemonDataBeforeRedirect(pokemonID);
+            if (success) {
+                window.location.href = `./detail.html?id=${pokemonID}`;
+            }
+        });
+
+        listWrapper.appendChild(listItem)
+    });
+}
+
+    searchInput.addEventListener("keyup", handlesearch);
+
+    function handlesearch() {
+        const searchTerm = searchInput.value.toLowerCase();
+        let filteredPokemons; 
+    
+        if(numberFilter.checked) {
+            filteredPokemons = allPokemons.filter((pokemon) => {
+                const pokemonID = pokemon.url.split("/")[6];
+                return pokemonID.startsWith(searchTerm);
+            });
+        } else if(nameFilter.checked) {
+            filteredPokemons = allPokemons.filter((pokemon) => {
+                return pokemon.name.toLowerCase().startsWith(searchTerm);
+            });
+    } else {
+      filteredPokemons = allPokemons;
+    }
+
+    displayPokemons(filteredPokemons);
+
+    if(filteredPokemons.length ===  0) {
+        notFoundMessage.style.display = "block";
+    } else {
+        notFoundMessage.style.display = "none";
+    }
+}
+
+const closeButton = document.querySelector(".search-close-icon");
+closeButton.addEventListener("click", clearSearch);
+
+function clearSearch() {
+    searchInput.value = "";
+    displayPokemons(allPokemons);
+    notFoundMessage.style.display = "none";
 }
